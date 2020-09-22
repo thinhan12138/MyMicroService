6f1cc07d7cad83ef5f171809cf44915d8efbc4d9
thinhan12138 <han_thin@163.com>
2020-09-22 14:17:41 +0800

    3、2020/09/22

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 基于微服务架构的综合项目
 
 基础服务：
-base-auth:登录、鉴权、安全认证（shiro）
+base-auth:登录、鉴权、安全认证（SpringSecurity、OAuth2）
 base-message：消息收发、消息管理（kafka）
 base-flow：flowyable
 base-user:
@@ -17,3 +17,8 @@ Spring扩展点：
 2、
 3、
 
+功能模块：
+1、动态生成接口；
+2、异步执行器，动态监听配置文件，参照Nacos配置中心；
+3、分布式定时任务实现；
+4、高并发请求，秒杀模块；
--- core/src/main/java/microservice/core/CoreApplication.java
+++ core/src/main/java/microservice/core/CoreApplication.java
@@ -1,7 +1,5 @@
 package microservice.core;
 
-import microservice.core.entity.User;
-import microservice.core.mapper.UserMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class CoreApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(CoreApplication.class, args);
-        User user = (User) applicationContext.getBean("user");
-        UserMapper userMapper = (UserMapper) applicationContext.getBean("userMapper");
-        System.out.println(user);
-        userMapper.insert(user);
     }
 
 }
